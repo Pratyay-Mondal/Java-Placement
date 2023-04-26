852b469eb344b8667c6b182e01f7389b01ba4179
Pratyay Mondal <81563083+Pratyay008@users.noreply.github.com>
2023-04-26 23:28:51 +0530

    Create 230. Kth Smallest Element in a BST.java

---
+++ 230. Kth Smallest Element in a BST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution {
+    ArrayList<Integer> li = new ArrayList<>();
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root);
+        return li.get(k-1);     
+    }
+
+
+    public void inorder (TreeNode root){
+        if(root == null) return;
+
+        inorder(root.left);
+        li.add(root.val);
+        inorder(root.right);
+    }
+
+}
+

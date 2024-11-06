a8a5dc96a74b50b85892b4eecf088f2aaac174d9
Pratyay Mondal <81563083+Pratyay-Mondal@users.noreply.github.com>
2024-11-06 19:42:27 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,24 +3,27 @@ import java.util.*;
 class Team{
     
     int id;
-    String nm;
-    String loc;
+    String name;
+    String location;
     int runs;
-    int wkt;
+    int wickets;
     
-    public Team(int id, String nm, String loc, int runs, int wkt){
-        this.id = id;
-        this.nm = nm;
-        this.loc = loc;
-        this.runs = runs;
-        this.wkt = wkt;
+    
+    public Team(int id, String name, String location, int runs, int wickets){
+        this.id=id;
+        this.name=name;
+        this.location=location;
+        this.runs=runs;
+        this.wickets=wickets;
+        
     }
-}    
-    
-    
+}   
+
+
 class Solution{
     
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         Team arr[] = new Team[3];
         
@@ -28,63 +31,50 @@ class Solution{
         for(int i=0; i<3; i++){
             int a = sc.nextInt();
             sc.nextLine();
-            String b = sc.nextLine();
-            String c = sc.nextLine();
+            String b= sc.nextLine();
+            String c= sc.nextLine();
             int d = sc.nextInt();
             int e = sc.nextInt();
             sc.nextLine();
-            
-            arr[i] = new Team(a, b, c, d, e);
+        
+        
+            arr[i] = new Team(a,b,c,d,e);
         }
         
-        
-        String str = sc.nextLine();  
-        int n = sc.nextInt();
+        String str = sc.nextLine();
+        int val = sc.nextInt();
         sc.nextLine();
-        int res = hRuns(arr, str, n);
         
-        if(res>0){
-            System.out.println(res);
+        int sum = highRuns(arr, str, val);
+        
+        if(sum>0){
+            System.out.println(sum);
         }
         else{
-            System.out.println("No any resort");
+            System.out.println("NA");
         }
-        
-        //System.out.println("Hey! there, I am Brie.");
-        
     }
     
     
-    public static int hRuns(Team arr[], String str, int n){
-        int sum=0;
+    
+    public static int highRuns(Team arr[], String str, int val){
         int temp=0;
+        int res=0;
         
-        for(int i=0; i<arr.length; i++){
-            if(arr[i].loc.equalsIgnoreCase(str) && arr[i].wkt > n){
+        for(int i=0; i<3; i++){
+            if(arr[i].location.equals(str) && arr[i].wickets>val){
                 temp = arr[i].runs;
-                if(temp > sum){
-                    sum = temp;
+                if(temp>res){
+                    res = temp;
                 }
             }
         }
-        return sum;
+        
+        return res;
     }
     
     
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+}
 
